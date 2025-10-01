7cff457caadc034b1b95c2e1e30e506ca5cadb60
mhdgilangalfaiz <mhdgilangalfaiz@gmail.com>
2025-10-01 13:38:08 +0700

    ADD CRUD Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,64 +9,61 @@ use Illuminate\Validation\ValidationException;
 class ProductController extends Controller
 {
     /**
-     * Tampilkan semua data produk.
+     * Tampilkan semua data Produk.
      * Beserta pemiliknya (user)
      */
     public function index()
     {
-
-    }
-
-    /**
-     * cari produk berdasarkan nama
-     * dan ikuti relasinya
-     */
-    public function search(Request $req)
-    {
-        try {
-            // validasi minimal 3 huruf buat pencarian
-            $validated = $req->validate([
-                'teks' => 'required|min:3',
-            ], [
-                // pesan eror costum
-                'teks.required' => ':Attribute jangan dikosngkan lah!',
-                'teks.min' => 'Ini :attribut kurang dari :min bos!',
-            ], [
-                //custom attributs
-                'teks' => 'huruf'
-            ]);
-
-            // proses pencarian produk berdasarkan teks yang dikirim
-            $products = Product::query()
-                ->where('name', 'like', '%' . $req->teks . '%')
-                ->with('user')
-                ->get();
-            // return hasil pencarian
-            return response()->json([
-                'pesan' => 'Sukses!',
-                'data' => $products,
-            ]);
-        }
-        //use Illuminate\Validation\ValidationException;
-        catch (ValidationException $ex) {
-            return response()->json([
-                'pesan' => 'Gagal',
-                'data' => $ex->getMessage(),
-            ]);
-        }
-        // untuk memanggil relasi terkait, sebutkan
-        // nama method relasi yang ada di model tersebut 
-        // Gunakan  method with() untuk menyertakn relasi table
-        // pada data yang di panggil
+        // untuk memanggil relasi terkait, sebutkan 
+        // nama method yang ada di model tersebut.
+        // gunakan method with() untuk menyertakan relasi tabel
+        // pada data yang dipanggil.
         $products = Product::query()
-            ->where('is_available', true)
-            ->with('user')
-            ->get();
-        // Format respon ada status (sukses/gagal) dan data
+            ->where('is_available', true) // Hanya produk tersedia
+            ->with('user')                // sertakan pemiliknya
+            ->get();                      // eksekusi query
+        // format respon ada status (Sukses/Gagal) dan data
         return response()->json([
             'status' => 'Sukses',
             'data' => $products
         ]);
+    }
+
+    /**
+     * Cari produk berdasarkan 'name'
+     * dan ikutkan relasinya
+     */
+    public function search(Request $req)
+    {
+        try {
+            // validasi minimal 3 huruf untuk pencarian
+            $validate = $req->validate([
+                'teks' => 'required|min:3',
+            ], [
+                // pesan error costum
+                'teks.required' => ':Attribute jangan dikosongkan',
+                'teks.min' => 'Ini :attribute kurang dari :min',
+            ], [
+                // costum attribute
+                'teks' => 'huruf'
+            ]);
+
+            //proses pencarian produk berdasarkan teks yang dikirim
+            $products = Product::query()
+                ->where('name', 'like', '%' . $req->teks . '%')
+                ->with('user')
+                ->get();
+            return response()->json([
+                'pesan' => 'Sukses!',
+                'data' => $products,
+            ]);
+
+        } catch (ValidationException $ex) {
+            return response()->json([
+                'pesan' => 'Gagal!',
+                'data' => $ex->getMessage(),
+            ]);
+        }
     }
 
     /**
@@ -82,15 +79,59 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validate = $request->validate(
+                [
+                    'user_id' => 'nullable|exists:users,id',
+                    'name' => 'required|string|max:255',
+                    'image_path' => 'nullable|string',
+                    'stock' => 'required|integer',
+                    'price' => 'required|numeric',
+                    'description' => 'required|string',
+                ],
+                [
+                    'name.required' => 'Nama produk jangan dikosongkan',
+                    'price.required' => 'Harga wajib diisi',
+                    'price.numeric' => 'Harga harus berupa angka',
+                    'stock.required' => 'Stok wajib diisi',
+                    'stock.integer' => 'Stok harus berupa angka'
+                ]
+            );
+
+            $product = Product::create($validate);
+
+            return response()->json([
+                'status' => 'Sukses',
+                'data' => $product
+            ]);
+
+        } catch (ValidationException $ex) {
+            return response()->json([
+                'pesan' => 'Gagal!',
+                'data' => $ex->getMessage(),
+            ]);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    public function show($id)
     {
-        //
+        try {
+            // cari produk berdasarkan id
+            $product = Product::with('user')->findOrFail($id);
+
+            return response()->json([
+                'status' => 'Product ini ada!',
+                'data' => $product
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'Gagal',
+                'pesan' => 'Produk dengan ID ' . $id . ' tidak ditemukan',
+            ], 404);
+        }
     }
 
     /**
@@ -104,16 +145,64 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $r, $id)
     {
-        //
+        try {
+            // untuk mencari produk berdasarkan id
+            $product = Product::findOrFail($id);
+
+            $validate = $r->validate([
+                'name' => 'string|max:22',
+                'price' => 'numeric|min:0',
+                'image_path' => 'nullable|string',
+                'stock' => 'integer|min:0',
+                'description' => 'nullable|string',
+            ]);
+            $product->update([
+                'name' => $data['name'] ?? $product->name,
+                'price' => $data['price'] ?? $product->price,
+                'image_path' => $data['image_path'] ?? $product->image_path,
+                'stock' => $data['stock'] ?? $product->stock,
+                'description' => $data['description'] ?? $product->description,
+            ]);
+            $product->update(array_filter($validate, function ($value) {
+                return $value !== null;
+            }));
+
+            return response()->json([
+                'status' => 'Produk Berhasil Diubah',
+            ]);
+
+        } catch (ValidationException $ex) {
+            return response()->json([
+                'pesan' => 'Gagal Mengubah Data',
+                'error' => $ex->validator->errors(),
+            ]);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy($id)
     {
-        //
+        try {
+            // Cari produk berdasarkan id
+            $product = Product::findOrFail($id);
+
+            // Hapus produk
+            $product->delete();
+
+            return response()->json([
+                'status' => 'Sukses',
+                'message' => 'Produk berhasil dihapus',
+            ]);
+
+        } catch (\Exception $ex) {
+            return response()->json([
+                'status' => 'Gagal',
+                'error' => $ex->getMessage()
+            ], 404);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,78 +2,92 @@
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\ValidationException as ValidationException;
+use Illuminate\Validation\ValidationException;
 
 // default endpoint API: http://api-5sia1.test/api
-// route ambil semua data user
-//method: GET
 
 // Product Controller
-// Menampung semua logika dan perintah yangdiarahkan dari endpoint url berikut
+// menampung semua logika dan perintah yang diarahkan
+// dari endpoint url di sini (api.php).
 use App\Http\Controllers\ProductController;
 
 /**
- * API Resource untuk model product
+ * API Resource untuk model Product
  */
-// 1. Ambil semua produk beserta pemiliknya (user)
+// 1. Ambil semua data Produk beserta pemiliknya (user)
 // action url = [NamaController::class, 'method']
 Route::get('/products/semuanya', [ProductController::class, 'index']);
 // 2. Cari produk tersedia berdasarkan nama
 Route::get('/products/cari', [ProductController::class, 'search']);
+// 3. Nambah produk
+Route::post('/products/tambah', [ProductController::class, 'store']);
+// 4. read detail produk berdasarkan id
+Route::get('/products/{id}', [ProductController::class, 'show']);
+// 5. Update
+Route::put('/products/update/{id}', [ProductController::class, 'update']);
+// 6. Hapus
+Route::delete('/products/delete/{id}', [ProductController::class, 'destroy']);
 
 
+
+
+// route ambil semua data user
+// method: GET
 Route::get('/users', function () {
-    //panggil semua data user dan simpan dalam variabel $users
-    //Method with() digunakan untuk mengikutsertakan relasi
-    //relasi yang disebutkan sesuai dengan nama method pada model
+    // panggil semua data user dan simpan dalam variabel $users
+    // method with() digunakan untuk mengikutsertakan relasi
+    // relasi yang disebutkan sesuai dengan nama method pada model
     $users = User::query()->with('products')->get();
-    //convert ke dalam format JSON
+    // convert ke dalam format JSON
     $json_users = json_encode($users);
-    //berikan data (response) json ke aplikasi yang meminta (request)
+    // berikan data (response) json ke aplikasi yang meminta (request)
     return $json_users;
 });
 
 // route cari user berdasarkan id
 // method: GET
 Route::get('/user/find', function (Request $request) {
-    //cari user
+    // cari user tanpa relasi ke tabel products
     // $user = User::find($request->id);
     // cari user dengan relasi
     $user = User::query()
         ->where('id', $request->id)
         ->with('products')
         ->get();
+    // dd($user); // dump and die
     return json_encode($user);
 });
 
 // route cari user berdasarkan kemiripan nama atau email
 // method: GET
-
 Route::get('/user/search', function (Request $request) {
     // cari user berdasarkan string nama
-    $users = User::where('nama', 'like', '%' . $request->name . '%')
-        ->orWhere('email', 'like', '%' . $request->nama . '%')->get();
+    $users = User::where('name', 'like', '%' . $request->nama . '%')
+        ->orWhere('email', 'like', '%' . $request->nama . '%')
+        ->get();
     // SELECT * FROM users WHERE name OR email LIKE '%ahmad%';
     return json_encode($users);
 });
 
+
 // Registrasi User
-// Parameter name,email,phone,password
+// Parameter name, email, phone, password
 // password harus di hash sebelum disimpan ke tabel
 Route::post('/register', function (Request $r) {
-    //validasi data
     try {
+        // validasi data
         $validated = $r->validate([
-            //param => rules
+            // params => rules
             'nama' => 'required|max:255',
             'surel' => 'required|email|unique:users,email',
             'telp' => 'required|unique:users,phone',
             'sandi' => 'required|min:6'
         ]);
-        //tambahkan data user baru
+        // tambahkan data user baru
         $new_user = User::query()->create([
-            //field => params
+            // field => params
             'name' => $r->nama,
             'email' => $r->surel,
             'phone' => $r->telp,
@@ -86,48 +100,53 @@ Route::post('/register', function (Request $r) {
     }
 });
 
+
 // Ubah Data User
-// Parameter name,surel,telp,sandi
-// method 'PUT' atau 'PATCH'
-// data user yang diubah berdasarkan id yang di kirim
-// pada contoh ini , id akan di langsung di asosiasikan ke model User
+// parameter nama, surel, telp, sandi
+// method `PUT` atau `PATCH`
+// data user yang akan diubah dicari berdasarkan id yang dikirim
+// pada contoh ini, id akan langsung diasosiasikan ke model User
 Route::put('/user/edit/{user}', function (Request $r, User $user) {
+
     try {
-        //validasi ubah data
+        // validasi ubah data
         $validated = $r->validate([
             'nama' => 'max:255',
             'surel' => 'email|unique:users,email,' . $user->id,
             'telp' => 'unique:users,phone,' . $user->id,
-            'sandi' => 'nullable|min:6'
+            'sandi' => 'min:6'
         ]);
-        //==================================
-        // CARA SEDERHANA
-        //==================================
+
+        // ----------- cara yang sederhana
         // $user->update([
         //     'name'      => $r->nama ?? $user->name,
         //     'email'     => $r->surel ?? $user->email,
         //     'phone'     => $r->telp ?? $user->phone,
-        //     // jika ada data password, maka hash dan simpan
-        //     // jika tidak ada, gunakan password lama
-        //     'password'  => $r->sandi ? Hash::make($r->sandi) : $user->password
+        //     'password'  => $r->sandi
+        //                     ? Hash::make($r->sandi)
+        //                     : $user->password
         // ]);
-        //==================================
 
-        // salin data yang diterima ke variable baru
+        // ----------- cara yang kompleks
+        // salin data yang diterima ke variabel baru
         $data = $r->all();
         // jika ada data password pada array $data
         if (array_key_exists('sandi', $data)) {
-            // replace isi 'sandi' dengan hasil hash 'sandi'
+            // replace isi `sandi` dengan hasil Hash `sandi`
             $data['sandi'] = Hash::make($data['sandi']);
         }
-        //ubah data user
+
+        // ubah data user
         $user->update([
-            'name' => $r->nama ?? $user->name,
-            'email' => $r->surel ?? $user->email,
-            'phone' => $r->telp ?? $user->phone,
+            'name' => $data['nama'] ?? $user->name,
+            'email' => $data['surel'] ?? $user->email,
+            'phone' => $data['telp'] ?? $user->phone,
             'password' => $data['sandi'] ?? $user->password
         ]);
-        // kembalikan data user yang sudah di ubah beserta pesan sukses
+        // ---------- berakhirnya cara yang kompleks
+
+
+        // kembalikan data user yang sudah diubah beserta pesan sukses
         return response()->json([
             'pesan' => 'Sukses diubah!',
             'user' => $user,
@@ -136,6 +155,7 @@ Route::put('/user/edit/{user}', function (Request $r, User $user) {
     } catch (ValidationException $e) {
         return $e->validator->errors();
     }
+
 });
 
 // Hapus Data User
@@ -147,16 +167,22 @@ Route::delete('/user/delete', function (Request $r) {
     // respon jika user tidak ditemukan
     if (!$user)
         return response()->json([
-            'pesan' => 'Gagal! user tidak ditemukan.',
+            'pesan' => 'Gagal! User tidak ditemukan.'
         ]);
 
-    //hapus data user
+    // hapus data user jika ada
     $user->delete();
     return response()->json([
-        'pesan' => 'Sukses! user berhasil dihapus.',
+        'pesan' => 'Sukses! User berhasil dihapus.'
     ]);
 });
+
+
+
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
+
+
+?>
